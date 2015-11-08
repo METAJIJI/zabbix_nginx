4b0737c5a0f8417bff6e6e3621a6ed87d7cceb9e
Denis Kadyshev <metajiji@gmail.com>
2015-11-09 01:40:29 +0600

    Improve send hostname via zabbix.

--- zabbix/scripts/nginx_status.sh
+++ zabbix/scripts/nginx_status.sh
@@ -5,21 +5,20 @@ TMP='/tmp/nginx-ping.tmp'
 ZABBIX_SENDER='/usr/bin/env zabbix_sender'
 CONFIG='/etc/zabbix/zabbix_agentd.conf'
 
-# WARNING: Correctly setup 'Hostname=' in config is REQUIRED!
 # REQUIRED binaries: GNU time, wget, awk, zabbix_sender.
 
 (/usr/bin/env time -f %e /usr/bin/env wget --no-http-keep-alive --quiet --timeout 9 -O - $URL) 2>$TMP | \
-	awk '/Active connections/ {active=int($NF)}
+	awk -v ct=$1 '/Active connections/ {active=int($NF)}
 		/ ([0-9]+) ([0-9]+) ([0-9]+)/ {accepts=int($1); handled=int($2); requests=int($3)}
 		/Reading:/ {reading=int($2); writing=int($4); waiting=int($6)}
 		END {
-			print "- nginx.connections.active", active;
-			print "- nginx.connections.reading", reading;
-			print "- nginx.connections.writing", writing;
-			print "- nginx.connections.waiting", waiting;
-			print "- nginx.accepts", accepts;
-			print "- nginx.handled", handled;
-			print "- nginx.requests", requests;
+			print ct, "nginx.connections.active", active;
+			print ct, "nginx.connections.reading", reading;
+			print ct, "nginx.connections.writing", writing;
+			print ct, "nginx.connections.waiting", waiting;
+			print ct, "nginx.accepts", accepts;
+			print ct, "nginx.handled", handled;
+			print ct, "nginx.requests", requests;
 		}' | $ZABBIX_SENDER --config $CONFIG \
 			--input-file - >/dev/null 2>&1
 
